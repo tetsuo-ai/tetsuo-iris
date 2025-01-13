317bbc8bc1573cc574f76e212a8636daf3bb7a35
Ricky Chon <rickychon99@gmail.com>
2025-01-13 16:41:05 -0500

    ui: move finance page components to app/finance

--- app/finance/page.tsx
+++ app/finance/page.tsx
@@ -1,5 +1,11 @@
-import { FinancePage } from "@/components/pages/FinancePage";
+import { TradingViewChart } from "@/components/TradingViewChart";
+import { WhalesTransactions } from "@/components/WhalesTransactions";
 
-export default function Finance() {
-  return <FinancePage />;
+export default function FinancePage() {
+  return (
+    <div className="flex flex-col gap-4">
+      <TradingViewChart />
+      <WhalesTransactions />
+    </div>
+  );
 }
