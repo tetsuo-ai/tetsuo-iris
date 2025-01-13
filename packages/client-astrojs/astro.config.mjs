d64408cecc68d88432c1188c20d88bbab148c448
Ricky Chon <rickychon99@gmail.com>
2025-01-13 17:42:48 -0500

    ui(astro): add TradingView widget

--- packages/client-astrojs/astro.config.mjs
+++ packages/client-astrojs/astro.config.mjs
@@ -7,4 +7,7 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
+  redirects: {
+    '/': '/finance',
+  },
 });
