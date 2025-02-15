f161531d868622ea0e91259a11892572b715f1ba
Ricky Chon <rickychon99@gmail.com>
2025-02-15 02:56:40 -0500

    wip(astro): new homepage

--- packages/client-astrojs/astro.config.mjs
+++ packages/client-astrojs/astro.config.mjs
@@ -7,7 +7,4 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
-  redirects: {
-    '/': '/finance',
-  },
 });
